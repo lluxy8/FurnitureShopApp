cc6bc93a8d49ffe19c9db0c3a3728eda0ac353bc
muratcan sakin <lluxysa@gmail.com>
2025-04-11 18:48:45 +0300

    DbMaxLenghtValues class

---
+++ src/Core/Common/Values/DbMaxLenghtValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Values
+{
+    public class DbMaxLenghtValues
+    {
+        public const int Name = 50;
+
+        public const int Password = 50;
+
+        public const int Description = 500;
+    }
+}
